e6ca3fd6503d98feae27ef84958c36b935133f51
Hazakura2024 <ytr240.jp@gmail.com>
2026-01-03 14:43:18 +0900

    feat: モバイル端末の時傾きを受け取るよう修正

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -7,14 +7,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // 重力加速度
     const float Gravity = 9.81f;
 
-        // 重力の適用具合
+    // 重力の適用具合
     public float gravityScale = 1.0f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,22 +23,34 @@ public class NewMonoBehaviourScript : MonoBehaviour
         // 重力ベクトルの初期化
         Vector3 vector = new Vector3();
 
-        // キーの入力を検知しベクトルを設定
-        vector.x = Input.GetAxis("Horizontal");
-        vector.z = Input.GetAxis("Vertical");
+        if (Application.isMobilePlatform)
+        {
+            // 加速度を受けとる
+            vector.x = Input.acceleration.x;
+            vector.y = Input.acceleration.y;
+            vector.z = Input.acceleration.z;
 
-        // 高さ方向はキーのz
-        if (Input.GetKey("z"))
-    {
-        vector.y = 1.0f;
-    }
-    else
-    {
-        vector.y = -1.0f;
-    }
+        }
+        else
+        {
+            // キーの入力を検知しベクトルを設定
+            vector.x = Input.GetAxis("Horizontal");
+            vector.z = Input.GetAxis("Vertical");
 
-    // シーンの重力を入力ベクトルの方向に合わせて変化させる
-    Physics.gravity = Gravity * vector.normalized * gravityScale;
+            // 高さ方向はキーのz
+            if (Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
+        }
+
+
+        // シーンの重力を入力ベクトルの方向に合わせて変化させる
+        Physics.gravity = Gravity * vector.normalized * gravityScale;
 
     }
 }
