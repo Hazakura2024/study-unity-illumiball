39f296ead92a524cfca33809d87751f40311163e
Hazakura2024 <ytr240.jp@gmail.com>
2026-01-03 14:39:00 +0900

    feat: z軸を十字キー上下で受け取るよう修正

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    // 重力加速度
+    const float Gravity = 9.81f;
+
+        // 重力の適用具合
+    public float gravityScale = 1.0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 重力ベクトルの初期化
+        Vector3 vector = new Vector3();
+
+        // キーの入力を検知しベクトルを設定
+        vector.x = Input.GetAxis("Horizontal");
+        vector.z = Input.GetAxis("Vertical");
+
+        // 高さ方向はキーのz
+        if (Input.GetKey("z"))
+    {
+        vector.y = 1.0f;
+    }
+    else
+    {
+        vector.y = -1.0f;
+    }
+
+    // シーンの重力を入力ベクトルの方向に合わせて変化させる
+    Physics.gravity = Gravity * vector.normalized * gravityScale;
+
+    }
+}
